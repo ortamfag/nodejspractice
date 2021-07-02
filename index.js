d046a507230a8e27ea9529c816a843e8cfe70c3d
ortamfag <a_zhidov@outlook.com>
2021-07-02 16:43:47 +0300

    ADD: create routes

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express') //подключение express
 const mongoose = require('mongoose') //подключение mongoose
 const exphbs = require('express-handlebars') //подключение шаблонизатора
+const portfolioRoutes = require('./routes/portfolio') //подключение routes
 
-const PORT = 3000 //на каком порту идет запуск
+const PORT = 3002 //на каком порту идет запуск
 
 const app = express() //создание объекта приложения
-const hbs = exphbs.create.create({
+const hbs = exphbs.create({
     defaultLayout: 'main',
     extname: 'hbs' //смена формата (расширение после точки теперь будет .hbs)
 })
@@ -14,9 +15,13 @@ app.engine('hbs', hbs.engine) //регистрация движка hbs
 app.set('view engine', 'hbs')
 app.set('views', 'views')
 
+app.use(portfolioRoutes)
+
 async function start() { //подключение базы данных mongoose
     try{
-        await mongoose.connect('', {
+        await mongoose.connect(
+            'mongodb+srv://ortamfag:1QAZxsw23edc@portfolio.7ks7u.mongodb.net/portfolio', 
+        {
             useNewUrlParser: true,
             useFindAndModify: false
         })
