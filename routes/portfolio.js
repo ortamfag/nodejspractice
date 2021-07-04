48eadf901aaec2933730b89c5ea938a3ce84e4e3
Екатерина Ракитова <>
2021-07-04 13:04:05 +0300

    ADD: other title for pages

--- routes/portfolio.js
+++ routes/portfolio.js
@@ -1,8 +1,16 @@
 const { Router } = require('express') //возможность создания путей
 const router = Router()
 
-router.get('/', (req, res) => {
-    res.render('index')
+router.get('/', (req, res) => { //рендер страницы index
+    res.render('index', {
+        title: 'Portfolio list' //создание title для страницы index
+    }) 
+})
+
+router.get('/create', (req, res) => { //рендер страницы create
+    res.render('create', {
+        title: 'Create portfolio' //создание title для страницы Index
+    })
 })
 
 module.exports = router //экспорт наружу
