7524a97cc13b57d9291b81afba7135e1d809379e
Екатерина Ракитова <>
2021-07-04 13:15:47 +0300

    ADD: if/else for navbar

--- routes/portfolio.js
+++ routes/portfolio.js
@@ -3,13 +3,15 @@ const router = Router()
 
 router.get('/', (req, res) => { //рендер страницы index
     res.render('index', {
-        title: 'Portfolio list' //создание title для страницы index
+        title: 'Portfolio list', //создание title для страницы index
+        isIndex: true
     }) 
 })
 
 router.get('/create', (req, res) => { //рендер страницы create
     res.render('create', {
-        title: 'Create portfolio' //создание title для страницы Index
+        title: 'Create portfolio', //создание title для страницы Index
+        isCreate: true
     })
 })
 
